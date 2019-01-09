87562a8b8757297eb2e8aeec38666990f0c33063
Chris Tacey-Green <chris@taceygreen.com>
2019-01-09 13:11:40 +0000

    Add project files.

---
+++ PrefixSum_Divisible/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int A, int B, int K)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+
+        if (A % K == 0) {
+            return (B - A) / K + 1;
+        }
+        else {
+            return (B - (A - A % K)) / K;
+        }
+    }
+}
